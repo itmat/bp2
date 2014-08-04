6c896de54bf55a78bc30f87f40990c307703832d
Katharina Hayer <katharinaehayer@gmail.com>
2014-08-04 16:07:40 -0400

    master version

--- includes/nav.php
+++
@@ -1,8 +0,0 @@
-<div id="nav">
-
-  <a href="#">Home</a>
-  <a href="#">About</a>
-  <a href="#">Portfolio</a>
-  <a href="#">Contact</a>
-
-</div> <!-- end #nav -->
--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
 <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 
-<title>1stWebDesigner PHP Template</title>
+<title>Benchmarking Project 2</title>
 
 </head>
 
@@ -23,50 +23,20 @@
 
 <div id="wrapper">
 
-<?php include('includes/header.php'); ?>
-<?php include('includes/nav.php'); ?>
-
 
 <div id="content">
 
-<h1>Heading1</h1>
-<h2>Heading2</h2>
-<h3>Heading3</h3>
-<h4>Heading4</h4>
-<h5>Heading5</h5>
-
-<h3>Paragraph Element</h3>
-
 <p>
 
-Quisque pellentesque sodales aliquam. Morbi mollis neque eget arcu egestas non ultrices neque volutpat. Nam at nunc lectus, id vulputate purus. In et turpis ac mauris viverra iaculis. Cras sed elit a purus ultrices iaculis eget sit amet dolor. Praesent ac libero dolor, id viverra libero. Mauris aliquam nibh vitae eros sodales fermentum. Fusce cursus est varius ante vehicula eget ultrices felis eleifend. Nunc pharetra rutrum nibh et lobortis. Morbi vitae venenatis velit.
+under construction
 
 </p>
 
-<p>
 
-Quisque pellentesque sodales aliquam. Morbi mollis neque eget arcu egestas non ultrices neque volutpat. Nam at nunc lectus, id vulputate purus. In et turpis ac mauris viverra iaculis. Cras sed elit a purus ultrices iaculis eget sit amet dolor. Praesent ac libero dolor, id viverra libero. Mauris aliquam nibh vitae eros sodales fermentum. Fusce cursus est varius ante vehicula eget ultrices felis eleifend. Nunc pharetra rutrum nibh et lobortis. Morbi vitae venenatis velit.
-
-</p>
-
-<h3>Another Heading Starting Point</h3>
-
-<p>
-
-Quisque pellentesque sodales aliquam. Morbi mollis neque eget arcu egestas non ultrices neque volutpat. Nam at nunc lectus, id vulputate purus. In et turpis ac mauris viverra iaculis. Cras sed elit a purus ultrices iaculis eget sit amet dolor. Praesent ac libero dolor, id viverra libero. Mauris aliquam nibh vitae eros sodales fermentum. Fusce cursus est varius ante vehicula eget ultrices felis eleifend. Nunc pharetra rutrum nibh et lobortis. Morbi vitae venenatis velit.
-
-</p>
-
-<p>
-
-Quisque pellentesque sodales aliquam. Morbi mollis neque eget arcu egestas non ultrices neque volutpat. Nam at nunc lectus, id vulputate purus. In et turpis ac mauris viverra iaculis. Cras sed elit a purus ultrices iaculis eget sit amet dolor. Praesent ac libero dolor, id viverra libero. Mauris aliquam nibh vitae eros sodales fermentum. Fusce cursus est varius ante vehicula eget ultrices felis eleifend. Nunc pharetra rutrum nibh et lobortis. Morbi vitae venenatis velit.
-
-</p>
 
 </div> <!-- end #content -->
 
-<?php include('includes/sidebar.php'); ?>
-<?php include('includes/footer.php'); ?>
+
 
 </div> <!-- End #wrapper -->
 
