13a5412d801cfa5be71a7a569093ddc526974c1c
khayer <katharinaehayer@gmail.com>
2015-04-29 10:31:59 -0400

    hitting the pause button

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,6 @@
 <div id="nav">
 
-  <a href="/">Home</a>
+  <a href="index.php">Home</a>
   <!-- <a href="graph.php">Graphs</a> -->
   <a href="datasets.php">Datasets</a>
   <a href="software.php">Software</a>
