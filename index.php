8f11f3560425580b12b17cbf7f3acc957aedc6a6
khayer <katharinaehayer@gmail.com>
2015-05-04 16:22:36 -0400

    website done

--- index.php
+++ index.php
@@ -45,6 +45,7 @@
 
 <h4>Abstract: </h4>
 
+<!--
 <p>The advantages of RNA sequencing (RNA-Seq) suggest it will replace microarrays
   for highly parallel gene expression analysis. For example, in contrast to arrays,
   RNA-Seq is expected to be able to provide accurate identification and quantification
@@ -58,6 +59,11 @@
   complicating factors such as polymorphisms, intron signal, sequencing error, and
   multiple splice forms are present. These results point to the pressing need for
   further algorithm development. </p>
+-->
+<b>Motivation:</b> Because of the advantages of RNA sequencing (RNA-Seq) over microarrays, it is gaining widespread popularity for highly parallel gene expression analysis. For example, RNA-Seq is expected to be able to provide accurate identification and quantification of full-length splice forms. A number of informatics packages have been developed for this purpose, but short reads makes it a difficult problem in principle.  Sequenc-ing error and polymorphisms add further complications. It has become necessary to perform studies to determine which algorithms perform best and which if any algorithms perform adequately.   However, there is a dearth of independent and unbiased benchmarking studies. Here we take an approach using both simulated and experimental benchmark data to evaluate their accuracy.
+<br>
+<b>Results:</b> We conclude that most methods are inaccurate even using ideal-ized data, and that no method is highly accurate once multiple splice forms, polymorphisms, intron signal, sequencing errors, alignment errors, annota-tion errors, and other complicating factors are present. These results point to the pressing need for further algorithm development.
+
 
 <h4>Preprint is available at <a href="http://biorxiv.org/content/early/2014/07/14/007088">bioRxiv</a>.</h4>
 
